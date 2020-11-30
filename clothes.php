1fc5138e3cd3f21d333ecc01487d80222b063a7a
rerekrerek11 <74719752+rerekrerek11@users.noreply.github.com>
2020-11-30 12:29:58 +0200

    Update clothes.php

--- clothes.php
+++ clothes.php
@@ -23,12 +23,12 @@
                <div class="contanier">
 			   
 				<div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/00.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0025.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">plozes</div>
 					<a id="asd" href="pay.php"> $1800 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/9.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0023.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">jaket</div>
 					<a id="asd" href="pay.php"> $1500 </a>
 				  </div>
@@ -38,42 +38,42 @@
 					<a id="asd" href="pay.php"> $2300 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/8.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0021.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">accept</div>
 					<a id="asd" href="pay.php"> $450 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/2.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="pog.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">poqma</div>
 					<a id="asd" href="pay.php"> $230 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/3.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0024.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">JHDF</div>
 					<a id="asd" href="pay.php"> $3000 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/5.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="images.jpg"images alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">skert</div>
 					<a id="asd" href="pay.php"> $1020 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/6.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0024.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">adres</div>
 					<a id="asd" href="pay.php"> $445 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/9.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="تنزيل.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">kareena</div>
 					<a id="asd" href="pay.php"> $456 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/2.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="IMG-20201130-WA0026.jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">kop</div>
 					<a id="asd" href="pay.php"> $1750 </a>
 				  </div>
 				   <div class="gallery">                                               <!-- كود الصور كلنك مدمجه مع النص -->
-					  <img src="images/4.jpg" alt=" موجوده الصورة غير" width="100" height="80">
+					  <img src="talon..jpg" alt=" موجوده الصورة غير" width="100" height="80">
 					<div class="desc">talon</div>
 					<a id="asd" href="pay.php"> $223 </a>
 				  </div>
