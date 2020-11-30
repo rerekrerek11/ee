785ad50fd4e22e55b7f3307e70a80cde1793c417
rerekrerek11 <74719752+rerekrerek11@users.noreply.github.com>
2020-11-30 15:33:49 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,62 @@
+<center>
+    <a href="index.php">الصفحه الرئيسيه</a>
+    <h1>شاشه الدخول</h1>
+
+</center>
+<br>
+<br>
+
+<center>
+<form method="post">
+    username : <input type="text" name="username" required />
+    <br>
+   password : <input type="password" name="password" required />
+<br>
+<button name="login" type="submit">login</button>
+
+</form>
+
+
+</center>
+
+
+
+<?php
+$ussename="admin";
+$password="12345";
+
+if(isset($_POST['login'])){
+
+    $getUserName=$_POST['username'];
+    $getpassword=$_POST['password'];
+
+    if($ussename=== $getUserName && $password === $password){
+session_start();
+$_SESSION['USER'] = $getUserName;
+$_SESSION['PASSWORD'] = $getpassword;
+$_SESSION['LOGIN'] =true;
+
+  header("location: pay.php");
+    }else{
+        echo"<center>";
+        
+        echo"خطأ في اسم المستخدم او كلمه المرور";
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
+
+</body>
+</html>
---
+++ pay.php
@@ -0,0 +1,27 @@
+<center>
+    <a href="index.php">الصفحه الرئيسيه</a>
+    <h1>بيانات العميل</h1>
+
+</center>
+<br>
+<br>
+
+<center>
+<form method="post">
+  اسم العميل: <input type="text" name="" required />
+    <br>
+    <br>
+ الكميه المطلوبه: <input type="text" name="الكميه المطلوبه" required />
+<br>
+<br>
+العنوان: <input type="text" name="العنوان" required />
+<br>
+<br>
+رقم الهاتف: <input type="text" name="رقم الهاتف" required />
+<br>
+<br>
+<button name="ارسال" type="submit">ارسال البيانات</button>
+</form>
+
+
+</center>
